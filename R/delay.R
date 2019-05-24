d5f08db06b45ad8bbd68d366b5f245711dcf53ea
Henrik Singmann <singmann@gmail.com>
2019-05-24 20:15:46 +0100

    add new delay data plus preparation

--- R/delay.R
+++ R/delay.R
@@ -1,18 +1,45 @@
 #' Experiment 1 of Dunn, Newell, & Kalish (2012)
 #' 
-#' Participants in this study completed one of two category-learning tasks over
-#' 4 blocks of training (a within-participant factor) under one of 2 different
-#' conditions (a between-participant factor). The dependent variables are the
-#' proportions correct for each of two tasks defined according to the category
-#' structure that participants learned. For the rule-based (RB) group, the cate-
-#' gory structure was defined by a simple rule. For the information-integration
-#' (II) group, the category structure was more complex and could not be defined
-#' by a simple rule.
+#' Data from 130 `participant`s with two between-participants factors (`delay`
+#' and `structure`) with two levels each and one within-participants factor
+#' (`block`) with four levels. The dependent variable is proportion correct
+#' `pc`.
+#' 
+#' Participants in this study completed one of two category-learning tasks
+#' defined according to the category `structure` that participants learned. For
+#' the rule-based (RB) group, the category structure was defined by a simple
+#' rule. For the information-integration (II) group, the category structure was
+#' more complex and could not be defined by a simple rule.
+#' 
+#' The experiment consisted of four 80-trial `block`s for each participant. Within each block, all 80
+#' stimuli were presented in a random order (with different orders for each
+#' participant). Participants were told to learn which of four categories (labeled,
+#' 1, 2, 3, and 4) each stimulus belonged to. On each trial, a stimulus was
+#' presented, and participants terminated the display by pressing one of the
+#' keys labeled 1–4 on the computer keyboard corresponding to Categories 1–4,
+#' respectively. Following the response, a mask appeared. The length with which
+#' the mask was shown defined the `delay` condition. Either 0.5-s (No Delay
+#' condition) or 5-s (Delay condition).
+#' 
+#' Following presentation of the mask, feedback appeared on the computer screen
+#' for 0.75 s. If the response was correct, the word “Correct” was presented;
+#' otherwise, the word “Incorrect” was presented.  Following presentation of
+#' feedback, a blank screen followed the duration of which was again defined the
+#' `delay` condition. The screen was blank for either 5 s (No Delay condition)
+#' or 0.5 s (Delay condition) before the next trial commenced. The sequence and
+#' timing of these events were same as those used by Maddox and Ing (2005).
+#' 
 #'
 #' @source Dunn, J. C., Newell, B. R., & Kalish, M. L. (2012). The effect of
 #'   feedback delay and feedback type on perceptual category learning: The
 #'   limits of multiple systems. \emph{Journal of Experimental Psychology:
 #'   Learning, Memory, and Cognition}, 38(4), 840-859.
 #'   \url{https://doi.org/10.1037/a0027867}
+#'   
+#' @references Maddox, W. T., & Ing, A. D. (2005). Delayed Feedback Disrupts the
+#'   Procedural-Learning System but Not the Hypothesis-Testing System in
+#'   Perceptual Category Learning. *Journal of Experimental Psychology: Learning,
+#'   Memory, and Cognition*, 31(1), 100-107.
+#'   \url{https://doi.org/10.1037/0278-7393.31.1.100}
 #' @example examples/examples.delay.R
 "delay"
---
+++ development/prepare_delay_data.R
@@ -0,0 +1,42 @@
+
+library("tidyverse")
+
+## data from Exp. 1 of Dunn, Newell, & Kalish (2012)
+delay <- read.table("development/delay.dat")
+colnames(delay) <- c("participant", "condition", "dv", 
+                     "B1","B2","B3","B4")
+
+delay2 <- delay %>% 
+  mutate(participant = factor(participant),
+         delay = factor(condition, 
+                        levels = 1:2, 
+                        labels = c("no delay", "delay")),
+         structure = factor(dv, 
+                            levels = 1:2,
+                            labels = c("rule-based", 
+                                       "information-integration")))
+
+delay2 %>% 
+  group_by(structure, delay) %>% 
+  summarise(n())
+
+delay <- delay2 %>% 
+  select(-condition, -dv) %>% 
+  gather(key = "block", "pc", B1:B4) %>% 
+  mutate(block = factor(block))
+str(delay)
+# 'data.frame':	520 obs. of  5 variables:
+#  $ participant: Factor w/ 130 levels "1","2","3","4",..: 1 2 3 4 5 6 7 8 9 10 ...
+#  $ delay      : Factor w/ 2 levels "no delay","delay": 2 1 1 2 2 1 1 2 2 2 ...
+#  $ structure  : Factor w/ 2 levels "rule-based","information-integration": 1 2 1 2 1 2 1 2 1 1 ...
+#  $ block      : Factor w/ 4 levels "B1","B2","B3",..: 1 1 1 1 1 1 1 1 1 1 ...
+#  $ pc         : num  0.338 0.287 0.525 0.35 0.237 ...
+
+usethis::use_data(delay, overwrite = TRUE)
+
+prep_data(delay, 
+          col_value = "pc", 
+          col_participant = "participant",
+          col_dv = "structure", 
+          col_within = "block", 
+          col_between = "delay")
