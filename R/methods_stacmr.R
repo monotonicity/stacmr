d9a53451efa31763adaf8daff27f72101f7cd9bf
Henrik Singmann <singmann@gmail.com>
2020-01-09 09:25:08 +0000

    more modular print method

--- R/methods_stacmr.R
+++ R/methods_stacmr.R
@@ -1,14 +1,17 @@
 
 print.stacmr <- function (x, 
                           digits = max(3, getOption("digits") - 3), 
+                          varnames = TRUE,
                           ...) {
   cat("\nCMR fit to ", nrow(x$estimate), " data points with call:\n", 
       paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n",
       sep = "")
   
-  cat("DVs:", paste0(names(x$data_list[[1]]), collapse = " & "), "\n")
-  cat("Within:",  attr(x$data_list, "varnames")["within"], "\n")
-  cat("Between:",  attr(x$data_list, "varnames")["between"], "\n\n")
+  if (varnames) {
+    cat("DVs:", paste0(names(x$data_list[[1]]), collapse = " & "), "\n")
+    cat("Within:",  attr(x$data_list, "varnames")["within"], "\n")
+    cat("Between:",  attr(x$data_list, "varnames")["between"], "\n\n") 
+  }
   
   cat("Fit value (", attr(x, "value_fit"), "): ", 
       format(x$fit, digits = digits), 
@@ -24,15 +27,7 @@ summary.stacmr <- function(object,
                            digits = max(3, getOption("digits") - 3), 
                            ...) {
   
-  cat("\nCMR fit to ", nrow(object$estimate), " data points with call:\n", 
-      paste(deparse(object$call), sep = "\n", collapse = "\n"), "\n\n",
-      sep = "")
-  cat("Fit value (", attr(object, "value_fit"), "): ", 
-      format(object$fit, digits = digits), 
-      "\n", 
-      sep = "")
-  cat("p-value (based on", attr(object, "nsample"), "samples):",  
-      object$p, "\n\n")
+  print(object, digits = digits, varnames = FALSE)
   
   cat("Estimated cell means:\n")
   print.data.frame(object$estimate, digits = digits)
