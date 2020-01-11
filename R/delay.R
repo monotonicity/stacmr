b5b0a9e3372b1c1f83712f1daa8fb5e69d72b97b
Henrik Singmann <singmann@gmail.com>
2020-01-11 07:00:45 +0000

    reformatting

--- R/delay.R
+++ R/delay.R
@@ -4,23 +4,23 @@
 #' and `structure`) with two levels each and one within-participants factor
 #' (`block`) with four levels. The dependent variable is proportion correct
 #' `pc`.
-#' 
+#'
 #' Participants in this study completed one of two category-learning tasks
 #' defined according to the category `structure` that participants learned. For
 #' the rule-based (RB) group, the category structure was defined by a simple
 #' rule. For the information-integration (II) group, the category structure was
 #' more complex and could not be defined by a simple rule.
-#' 
-#' The experiment consisted of four 80-trial `block`s for each participant. Within each block, all 80
-#' stimuli were presented in a random order (with different orders for each
-#' participant). Participants were told to learn which of four categories (labeled,
-#' 1, 2, 3, and 4) each stimulus belonged to. On each trial, a stimulus was
-#' presented, and participants terminated the display by pressing one of the
-#' keys labeled 1-4 on the computer keyboard corresponding to Categories 1-4,
-#' respectively. Following the response, a mask appeared. The length with which
-#' the mask was shown defined the `delay` condition. Either 0.5-s (No Delay
-#' condition) or 5-s (Delay condition).
-#' 
+#'
+#' The experiment consisted of four 80-trial `block`s for each participant.
+#' Within each block, all 80 stimuli were presented in a random order (with
+#' different orders for each participant). Participants were told to learn which
+#' of four categories (labeled, 1, 2, 3, and 4) each stimulus belonged to. On
+#' each trial, a stimulus was presented, and participants terminated the display
+#' by pressing one of the keys labeled 1-4 on the computer keyboard
+#' corresponding to Categories 1-4, respectively. Following the response, a mask
+#' appeared. The length with which the mask was shown defined the `delay`
+#' condition. Either 0.5-s (No Delay condition) or 5-s (Delay condition).
+#'
 #' Following presentation of the mask, feedback appeared on the computer screen
 #' for 0.75 s. If the response was correct, the word "Correct" was presented;
 #' otherwise, the word "Incorrect" was presented.  Following presentation of
