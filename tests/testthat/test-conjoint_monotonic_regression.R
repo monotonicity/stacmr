b1062ce4e7dd17c091594c7ae74961e85eaf5eed
Henrik Singmann <singmann@gmail.com>
2020-01-11 05:41:07 +0000

    more extensive tests of delay data and print methods

--- tests/testthat/test-conjoint_monotonic_regression.R
+++ tests/testthat/test-conjoint_monotonic_regression.R
@@ -1,4 +1,4 @@
-test_that("mr() produces known results", {
+test_that("cmr() produces known results & basic output", {
   data(delay)
   ## basic version
   st_delay <- cmr(
@@ -18,6 +18,13 @@ test_that("mr() produces known results", {
   expect_equal(st_delay$fit,  1.7493, tolerance = 0.0002)
   expect_equal(st_delay$fit_diff,  1.5772, tolerance = 0.0002)
   
+  expect_output(print(st_delay), "CMR fit to 8 data points with call:")
+  expect_output(print(st_delay), "Fit difference to MR model \\(SSE\\):")
+  
+  expect_output(summary(st_delay), "CMR fit to 8 data points with call:")
+  expect_output(summary(st_delay), "Estimated cell means:")
+  expect_output(summary(st_delay), "rule.based information.integration within  between")
+  
   ## approximate version
   st_delay_a <- cmr(
     data = delay, 
@@ -34,4 +41,34 @@ test_that("mr() produces known results", {
   
   expect_equal(st_delay_a$fit,  1.7493, tolerance = 0.0002)
   expect_equal(st_delay_a$fit_diff,  1.5772, tolerance = 0.0002)
+  
+  expect_output(print(st_delay_a), "CMR fit to 8 data points with call:")
+  expect_output(print(st_delay_a), "Fit difference to MR model \\(SSE\\):")
+  
+  expect_output(summary(st_delay_a), "CMR fit to 8 data points with call:")
+  expect_output(summary(st_delay_a), "Estimated cell means:")
+  expect_output(summary(st_delay_a), "rule.based information.integration within  between")
+  
+  st_delay_nt <- cmr(
+    data = delay, 
+    col_value = "pc", 
+    col_participant = "participant",
+    col_dv = "structure", 
+    col_within = "block", 
+    col_between = "delay", 
+    test = FALSE,
+    partial = "auto" 
+  )
+  ### results from Dunn & Kalish section 5.2
+  
+  expect_equal(st_delay_nt$fit,  1.7493, tolerance = 0.0002)
+  expect_equal(st_delay_nt$fit_diff,  NA)
+  
+  expect_output(print(st_delay_nt), "CMR fit to 8 data points with call:")
+  expect_output(print(st_delay_nt), "Fit difference to MR model \\(SSE\\): NA")
+  
+  expect_output(summary(st_delay_nt), "CMR fit to 8 data points with call:")
+  expect_output(summary(st_delay_nt), "Estimated cell means:")
+  expect_output(summary(st_delay_nt), "rule.based information.integration within  between")
+  
 })
--- tests/testthat/test-monotonic_regression.R
+++ tests/testthat/test-monotonic_regression.R
@@ -1,4 +1,4 @@
-test_that("mr() produces known results", {
+test_that("mr() produces known results & basic output", {
   data(delay)
   
   ## basic version
@@ -34,5 +34,27 @@ test_that("mr() produces known results", {
   expect_gt(mr_d2$p, 0.72)
   
   expect_equal(mr_d2$fit,  0.1721, tolerance = 0.0002)
+  
+  mr_delay_nt <- mr(
+    data = delay, 
+    col_value = "pc", 
+    col_participant = "participant",
+    col_dv = "structure", 
+    col_within = "block", 
+    col_between = "delay", 
+    test = FALSE,
+    partial = "auto" 
+  )
+  ### results from Dunn & Kalish section 5.2
+  
+  expect_equal(mr_delay_nt$fit,  0.1721, tolerance = 0.0002)
+  expect_equal(mr_delay_nt$p,  NA)
+  
+  expect_output(print(mr_delay_nt), "MR fit to 8 data points with call:")
+  expect_output(print(mr_delay_nt), "p-value \\(based on 0 samples\\): NA")
+  
+  expect_output(summary(mr_delay_nt), "MR fit to 8 data points with call:")
+  expect_output(summary(mr_delay_nt), "Estimated cell means:")
+  expect_output(summary(mr_delay_nt), "rule.based information.integration within  between")
   
 })
